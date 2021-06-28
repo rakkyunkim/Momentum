2cc78268895487162eee7ce122c32248598e984d
rakkyunkim <59427067+rakkyunkim@users.noreply.github.com>
2021-06-28 15:49:49 -0700

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -6,7 +6,7 @@ const API_KEY = "c242577ae678b9f9481126925b0063fe";
 function onGeoSuccess(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url)
         .then((response) => response.json())
         .then((data) =>{
